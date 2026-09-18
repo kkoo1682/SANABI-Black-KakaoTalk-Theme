4c20ee9110231b27740b1918d27ff27f07564e21
kkoo1682 <kimhanho7474@gmail.com>
2026-09-18 16:46:14 +0900

    Add MainActivity with black theme and centered text

---
+++ app/src/main/java/com/sanabi/kakaotheme/MainActivity.java
@@ -0,0 +1,24 @@
+package com.sanabi.kakaotheme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("SANABI 블랙 테마");
+        textView.setTextColor(Color.WHITE);
+        textView.setBackgroundColor(Color.BLACK);
+        textView.setTextSize(24);
+        textView.setGravity(Gravity.CENTER);
+
+        setContentView(textView);
+    }
+}
